d97f2effb0dd0feef13266eb0959bbe697400cdc
Franklyn Tackitt <franklyn@tackitt.net>
2017-03-22 10:47:12 -0700

    Trim fields to max length

--- comanage_nacha/utils/entry_formatter.py
+++ comanage_nacha/utils/entry_formatter.py
@@ -2,9 +2,31 @@ import string
 
 
 class EntryFormatter(string.Formatter):
+    def _get_len_from_format_spec(self, format_spec):
+        toks = list(reversed(format_spec))
+        while toks:
+            c, toks = toks[0], toks[1:]
+            if not toks or str.isalpha(c) and str.isdigit(toks[0]):
+                break
+        else:
+            return None
+
+        digits = ''
+        while toks:
+            c, toks = toks[0], toks[1:]
+            digits = c + digits
+            if not toks or not str.isdigit(toks[0]):
+                break
+
+        if digits:
+            return int(digits)
 
     def format_field(self, value, format_spec):
-        return super(EntryFormatter, self).format_field(value, format_spec)
+        formatted = super(EntryFormatter, self).format_field(value, format_spec)
+        maxlen = self._get_len_from_format_spec(format_spec)
+        if maxlen and len(formatted) > maxlen:
+            return formatted[:maxlen]
+        return formatted
 
     def convert_field(self, value, conversion):
         if conversion == 'd':
--- tests/entries/test_entry_detail.py
+++ tests/entries/test_entry_detail.py
@@ -36,3 +36,18 @@ def test_reject():
     assert entry_detail.rejected
     assert '6030' == entry_detail.error_code
     assert "622507003908999999           0000010000               CUSTOMER ONE            0REJ060300000001" == entry_detail.dumps()
+
+
+def test_long_name():
+    entry_detail = EntryDetail()
+    entry_detail.transaction_code = 22
+    entry_detail.receiving_dfi_routing_number = '07640125'
+    entry_detail.routing_number_check_digit = 1
+    entry_detail.receiving_dfi_account_number = '1234567890'
+    entry_detail.amount = 55050
+    entry_detail.individual_id = '472727272'
+    entry_detail.individual_name = 'Saguaro Property Management'
+    entry_detail.addenda_record_indicator = 0
+    entry_detail.trace_number = 1
+
+    assert '6220764012511234567890       0000055050472727272      Saguaro Property Manag  0091000010000001' == entry_detail.dumps()
